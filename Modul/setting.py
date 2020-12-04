644f19ce51e20bb6a1a712a68cab5cfc0011533c
Zeidler-Elijah <zeidlerelijah@protonmail.com>
2020-12-04 20:20:33 +0100

    ähm

---
+++ Modul/setting.py
@@ -0,0 +1,25 @@
+# -------------------
+
+__author__ ='Fabian Stange'
+
+# ---- ---- import libraries ---- ----
+
+import os, sys
+# ---- ---- ---- ----
+
+# ---- ---- variables ---- ----
+# absolute path to working directory
+path = os.getcwd()
+
+# absolute path to Data
+path_Data = path+os.sep+'Modul'+os.sep+'Data'+os.sep
+# absolute path to saves
+path_Saves = path + os.sep + 'saves' + os.sep
+
+# ---- ---- ---- ----
+
+
+# ---- ---- code //  ---- ----
+print(path)
+print(path_Data)
+#print(path_Saves)
