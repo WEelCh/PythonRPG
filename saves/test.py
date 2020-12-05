d2a2c4a54ee582ac79759c15c3eea7c6e9561cdd
Zeidler-Elijah <zeidlerelijah@protonmail.com>
2020-12-05 19:51:46 +0100

    Abend 1 Patch

--- saves/test.py
+++ saves/test.py
@@ -1,19 +1,5 @@
-
-switch : int=0
-
-switch = int(0)
-
-
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('savegame_1.xml')
+tree = ET.parse('savegame_2.xml')
 root = tree.getroot()
 
-
-
-for i in root.findall('world/region'):
-    print(i.attrib["coord"])
-#change = root.find('world/region/small_tiles/tile')
-#ET.SubElement(change, 'tile')
-
-#tree.write('savegame_1.xml')
